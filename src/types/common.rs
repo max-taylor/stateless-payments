c9f3c7bec7123729b9be079e5a4e71f1c97fad3a
max-taylor <39660195+max-taylor@users.noreply.github.com>
2024-12-06 09:04:25 +1000

    fix: deserialising aggregate pub key

--- src/types/common.rs
+++ src/types/common.rs
@@ -1,3 +1,4 @@
+use base64::{engine::general_purpose::STANDARD, Engine as _};
 use blsful::inner_types::G1Projective;
 use blsful::{AggregateSignature, Bls12381G1Impl, BlsResult, PublicKey, SecretKey, Signature};
 use rand::rngs::StdRng;
@@ -34,12 +35,23 @@ impl<'de> Deserialize<'de> for BlsAggregateSignatureWrapper {
     where
         D: serde::Deserializer<'de>,
     {
-        let s = String::deserialize(deserializer)?;
-        let formatted_string = format!("\"{}\"", s);
+        // Deserialize the map
+        #[allow(non_snake_case)]
+        #[derive(Deserialize)]
+        struct MessageAugmentationWrapper {
+            MessageAugmentation: String,
+        }
 
-        let key: BlsAggregateSignature =
-            serde_json::from_str(&formatted_string).map_err(serde::de::Error::custom)?;
-        Ok(BlsAggregateSignatureWrapper(key))
+        let wrapper = MessageAugmentationWrapper::deserialize(deserializer)?;
+
+        // Parse the string into BlsSignature
+        let key: G1Projective =
+            serde_json::from_str(&format!("\"{}\"", wrapper.MessageAugmentation))
+                .map_err(serde::de::Error::custom)?;
+
+        Ok(BlsAggregateSignatureWrapper(
+            BlsAggregateSignature::MessageAugmentation(key),
+        ))
     }
 }
 
@@ -191,7 +203,7 @@ impl Serialize for BalanceProofKey {
         S: serde::Serializer,
     {
         // Serialize key as a string, e.g., base64(root) + ":" + public_key JSON
-        let root_str = base64::encode(self.root);
+        let root_str = STANDARD.encode(&self.root);
         let public_key_str =
             serde_json::to_string(&self.public_key).map_err(serde::ser::Error::custom)?;
         let combined = format!("{}:{}", root_str, public_key_str);
@@ -213,7 +225,8 @@ impl<'de> Deserialize<'de> for BalanceProofKey {
             serde::de::Error::custom("Missing public_key part in BalanceProofKey")
         })?;
 
-        let root = base64::decode(root_str)
+        let root = STANDARD
+            .decode(root_str)
             .map_err(serde::de::Error::custom)?
             .try_into()
             .map_err(|_| serde::de::Error::custom("Invalid U8_32 length"))?;
